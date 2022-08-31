15b2b891eb2d59e908e7e9c14c7f2ac3ad86e319
shibamoto <silverbirder@gmail.com>
2022-08-31 22:04:27 +0900

    add stable sdk

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask
+import io
+import os
+import warnings
+
+from PIL import Image
 from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    stability_api = client.StabilityInference(
+        key=os.environ['STABILITY_KEY'], 
+        verbose=True,
+    )
+    answers = stability_api.generate(
+    prompt="houston, we are a 'go' for launch!"
+    )
+    # iterating over the generator produces the api response
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                warnings.warn(
+                    "Your request activated the API's safety filters and could not be processed."
+                    "Please modify the prompt and try again.")
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = Image.open(io.BytesIO(artifact.binary))
+                # save file
     return "Hello, World!"
