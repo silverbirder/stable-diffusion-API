6e7b9b2f9e671b60a763cc52879f6c3e9bb15e63
shibamoto <silverbirder@gmail.com>
2022-08-31 23:21:38 +0900

    set api interface

--- app.py
+++ app.py
@@ -6,23 +6,25 @@ from PIL import Image
 from dotenv import load_dotenv
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
-from flask import Flask
+from flask import Flask, request, send_file
 
 
 load_dotenv()
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'],
+    verbose=True,
+)
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    stability_api = client.StabilityInference(
-        key=os.environ['STABILITY_KEY'],
-        verbose=True,
-    )
+def generate():
+    req = request.args
+    propmt = req.get("prompt", "")
     answers = stability_api.generate(
-        prompt="houston, we are a 'go' for launch!"
+        prompt=propmt
     )
-    # iterating over the generator produces the api response
     for resp in answers:
         for artifact in resp.artifacts:
             if artifact.finish_reason == generation.FILTER:
@@ -30,6 +32,7 @@ def hello():
                     "Your request activated the API's safety filters and could not be processed."
                     "Please modify the prompt and try again.")
             if artifact.type == generation.ARTIFACT_IMAGE:
-                img = Image.open(io.BytesIO(artifact.binary))
-                img.save("hello.png")
-    return "Hello, World!"
+                return send_file(
+                    io.BytesIO(artifact.binary),
+                    mimetype='image/png'
+                )
